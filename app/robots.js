b10e9e03259ef181a8b6964ee87c0fb5ed53dc32
fibreglasspoolcheck <help@fibreglasspoolcheck.com.au>
2026-03-13 15:15:48 +1000

    Fix robots.js - disallow path now matches PascalCase route /RedFlagsGuideAccess

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,10 @@
 export default function robots() {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/red-flags-guide-access'],
-    },
-    sitemap: 'https://fibreglasspoolcheck.com.au/sitemap.xml',
-  }
+    return {
+          rules: {
+                  userAgent: '*',
+                  allow: '/',
+                  disallow: ['/RedFlagsGuideAccess'],
+          },
+          sitemap: 'https://fibreglasspoolcheck.com.au/sitemap.xml',
+    }
 }
